cc4ae310bdddea796f9d0653f2ea43256f881491
joohy <joohyun104@gmail.com>
2021-03-09 13:50:47 +0900

    0309 13:50

---
+++ WinformApp/PracticeWinApp/LabelTestApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabelTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LblAutoSize.Text = LblManualSize.Text = string.Empty;
+        }
+
+        private void BtnPushText_Click(object sender, EventArgs e)
+        {
+            string sample1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur qui eum rem assumenda quia, ut repudiandae deleniti laborum nemo veniam optio quisquam earum aperiam esse soluta eligendi unde dolore impedit.";
+            string sample2 = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, minus doloribus doloremque perferendis in nihil, nemo eos ipsa similique eaque veritatis corrupti aperiam fugiat omnis? Voluptatem voluptatibus vitae quisquam dicta!";
+
+            LblAutoSize.Text = sample1;
+            LblManualSize.Text = sample2;
+                
+        }
+    }
+}
---
+++ WinformApp/PracticeWinApp/PracticeWinApp/Views/FrmParent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticeWinApp.Views
+{
+    public partial class FrmParent : Form
+    {
+        
+        public FrmParent()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmParent_Load(object sender, EventArgs e)
+        {
+            this.ClientSize = new Size(500, 500);
+
+            
+        }
+    }
+}
