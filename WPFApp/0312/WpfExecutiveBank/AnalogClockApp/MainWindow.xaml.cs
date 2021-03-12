7cde39f69ed676b0f72c4ee3d961951bee557863
joohy <joohyun104@gmail.com>
2021-03-12 15:55:51 +0900

    0312 15:55

---
+++ WPFApp/0312/WpfExecutiveBank/AnalogClockApp/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AnalogClockApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Point center { get; set; }
+        public double Radius { get; set; }
+        public int HourHand { get; set; }
+        public int MinHand { get; set; }
+        public int SecHand { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            SetClock(); //시계 초기화
+            SetTimer(); //타이머 초기화
+
+        }
+
+        private void SetTimer()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10); //0.01초
+            //timer.Interval = new TimeSpan(0, 0, 1); //1초
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime curTime = DateTime.Now;
+
+            CvsClock.Children.Clear();
+            DrawClockFace(); //시계판 그리기
+
+            double radHour = (curTime.Hour % 12 + curTime.Minute / 60.6) * 30 * Math.PI / 180; //현재 시의 각도
+            double radMin = (curTime.Minute + curTime.Second / 60.0) * 6 * Math.PI / 180;
+            double radSec = (curTime.Second + curTime.Millisecond / 1000.0) * 6 * Math.PI / 180;
+            DrawHands(radHour, radMin, radSec); // 바늘 그리기
+        }
+
+        private void DrawHands(double radHour, double radMin, double radSec)
+        {
+            // 시침
+            DrawLine(HourHand * Math.Sin(radHour), -HourHand * Math.Cos(radHour),
+                0, 0, Brushes.RoyalBlue, 8, new Thickness(center.X, center.Y, 0, 0));
+            //분침
+            DrawLine(MinHand * Math.Sin(radMin), -MinHand * Math.Cos(radMin),
+                0, 0, Brushes.SkyBlue, 6, new Thickness(center.X, center.Y, 0, 0));
+            // 초침
+            DrawLine(SecHand * Math.Sin(radSec), -SecHand * Math.Cos(radSec),
+                0, 0, Brushes.OrangeRed, 3, new Thickness(center.X, center.Y, 0, 0));
+
+            Ellipse core = new Ellipse();
+            core.Margin = new Thickness(CvsClock.Width / 2 - 10,
+                CvsClock.Height / 2 - 10, 0, 0);
+            core.Stroke = Brushes.SteelBlue;
+            core.Fill = Brushes.LightSteelBlue;
+            core.Width = 18;
+            core.Height = 18;
+            CvsClock.Children.Add(core);
+        }
+
+        private void DrawLine(double x1, double y1, int x2, int y2,
+        SolidColorBrush color, int thick, Thickness margin)
+        {
+            Line line = new Line();
+            line.X1 = x1; line.Y1 = y1; line.X2 = x2; line.Y2 = y2;
+            line.Stroke = color;
+            line.StrokeThickness = thick;
+            line.Margin = margin;
+            line.StrokeStartLineCap = PenLineCap.Round;
+            CvsClock.Children.Add(line);
+        }
+
+        private void DrawClockFace()
+        {
+            ElsClock.Stroke = Brushes.LightSteelBlue;
+            ElsClock.StrokeThickness = 30;
+            CvsClock.Children.Add(ElsClock);
+        }
+
+        private void SetClock()
+        {
+            center = new Point(CvsClock.Width / 2, CvsClock.Height / 2); //시계 중심
+            Radius = CvsClock.Width / 2; //반지름
+            HourHand = (int)(Radius * 0.45); //시침
+            MinHand = (int)(Radius * 0.55); //분침
+            SecHand = (int)(Radius * 0.65); //초침
+        }
+    }
+}
---
+++ WinformApp/0312/WinExecutiveBank/MyNotePadApp/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyNotePadApp
+{
+    public partial class Form1 : Form
+    {
+        public bool isModify { get; set; } //flag
+        private const string firstFileName = "noName.txt"; //initial file name
+        private string currFileName = firstFileName;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MnuNewFile_Click(object sender, EventArgs e)
+        {
+            //작업중인 파일이 있으면 저장처리
+            BeforeClose();
+
+            TxtMain.Text = "";
+            this.Text = $"{currFileName} - 내 메모장";
+            isModify = false;
+            currFileName = firstFileName;
+        }
+
+        private void BeforeClose()
+        {
+            if (isModify)
+            {
+                DialogResult answer = MessageBox.Show("저장하시겠습니까?", "저장확인", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (answer == DialogResult.Yes)
+                {
+                    if (currFileName == firstFileName)
+                    {
+                        if (DlgSaveText.ShowDialog() == DialogResult.OK)
+                        {
+                            StreamWriter sw = File.CreateText(DlgSaveText.FileName);
+                            sw.WriteLine(TxtMain.Text);
+                            sw.Close();
+                        }
+                        else
+                        {
+                            StreamWriter sw = File.CreateText(currFileName);
+                            sw.WriteLine(TxtMain.Text);
+                            sw.Close();
+                        }
+                    }
+                }               
+            }
+        }
+
+        private void MnuOpenFile_Click(object sender, EventArgs e)
+        {
+            BeforeClose();
+            DlgOpenText.ShowDialog();
+            currFileName = DlgOpenText.FileName;
+            this.Text = $"{currFileName} - 내 메모장";
+
+            try
+            {
+                StreamReader sr = File.OpenText(currFileName);
+                TxtMain.Text = sr.ReadToEnd();
+                isModify = false;
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void MnuSaveFile_Click(object sender, EventArgs e)
+        {
+            if (currFileName == firstFileName)
+            {
+                if (DlgSaveText.ShowDialog() == DialogResult.OK)
+                    currFileName = DlgSaveText.FileName;
+            }
+
+            StreamWriter sw = File.CreateText(currFileName);
+            sw.WriteLine(TxtMain.Text);
+
+            isModify = false;
+            sw.Close();
+
+            this.Text = $"{currFileName} - 내 메모장";
+        }
+
+        private void MnuExit_Click(object sender, EventArgs e)
+        {
+            BeforeClose();
+            Environment.Exit(0);
+        }
+
+        private void MnuCopy_Click(object sender, EventArgs e)
+        {
+            var contents = ActiveControl as RichTextBox;
+            if (contents != null)
+            {
+                Clipboard.SetDataObject(contents.SelectedText);
+                MessageBox.Show(contents.SelectedText);
+            }
+        }
+
+        private void MnuPaste_Click(object sender, EventArgs e)
+        {
+            var contents = ActiveControl as RichTextBox;
+            if (contents != null)
+            {
+                IDataObject data = Clipboard.GetDataObject();
+                contents.SelectedText = data.GetData(DataFormats.Text).ToString();
+                isModify = true;
+            }
+        }
+
+        private void MnuAbout_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("메모장 v1.0입니다");
+            var form = new AboutThis();
+            form.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DlgSaveText.Filter = DlgOpenText.Filter = 
+                "Text file (*.txt)|*.txt|Log file (*.log)|*.log|Readme file (*.md)|*.md";
+            this.Text = $"{currFileName} - 내 메모장";
+            isModify = false;
+        }
+
+        private void TxtMain_TextChanged(object sender, EventArgs e)
+        {
+            isModify = true;
+            this.Text = '*' + $"{currFileName} - 내 메모장";
+        }
+    }
+}
