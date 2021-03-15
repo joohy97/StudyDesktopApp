be151d0da3ca391cde4fb1bc6a45b2ffef8ac627
joohy <joohyun104@gmail.com>
2021-03-15 14:27:39 +0900

    0315 14:27

---
+++ WinformApp/0315/WinChartApp/WinChartApp/Form2.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace WinChartApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.BtnMerge = new System.Windows.Forms.Button();
+            this.BtnSplit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(16, 12);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(700, 360);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // BtnMerge
+            // 
+            this.BtnMerge.Location = new System.Drawing.Point(225, 378);
+            this.BtnMerge.Name = "BtnMerge";
+            this.BtnMerge.Size = new System.Drawing.Size(117, 43);
+            this.BtnMerge.TabIndex = 1;
+            this.BtnMerge.Text = "합치기";
+            this.BtnMerge.UseVisualStyleBackColor = true;
+            this.BtnMerge.Click += new System.EventHandler(this.BtnMerge_Click);
+            // 
+            // BtnSplit
+            // 
+            this.BtnSplit.Location = new System.Drawing.Point(366, 378);
+            this.BtnSplit.Name = "BtnSplit";
+            this.BtnSplit.Size = new System.Drawing.Size(117, 43);
+            this.BtnSplit.TabIndex = 2;
+            this.BtnSplit.Text = "나누기";
+            this.BtnSplit.UseVisualStyleBackColor = true;
+            this.BtnSplit.Click += new System.EventHandler(this.BtnSplit_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 433);
+            this.Controls.Add(this.BtnSplit);
+            this.Controls.Add(this.BtnMerge);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button BtnMerge;
+        private System.Windows.Forms.Button BtnSplit;
+    }
+}
---
+++ WinformApp/0315/WinChartApp/WinChartApp/Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WinChartApp
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Text = "중간고사 성적2";
+
+            chart1.Titles.Add("중간고사 성적");
+            chart1.Series.Add("Series2");
+            chart1.Series["Series1"].LegendText = "수학";
+            chart1.Series["Series2"].LegendText = "영어";
+
+            chart1.ChartAreas.Add("ChartAreas2");
+            chart1.Series["Series2"].ChartArea = "ChartAreas2";
+
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                chart1.Series[0].Points.AddXY(i, rand.Next(10, 100));
+                chart1.Series[1].Points.AddXY(i, rand.Next(10, 100));
+            }
+
+            chart1.Series[0].ChartType = SeriesChartType.Column;
+            chart1.Series[1].ChartType = SeriesChartType.Line;
+
+            BtnSplit.Enabled = false;
+        }
+
+        private void BtnMerge_Click(object sender, EventArgs e)
+        {
+            chart1.ChartAreas.RemoveAt(chart1.ChartAreas.IndexOf("ChartAreas2"));
+            chart1.Series["Series2"].ChartArea = "ChartArea1";
+
+            BtnMerge.Enabled = false;
+            BtnSplit.Enabled = true;
+        }
+
+        private void BtnSplit_Click(object sender, EventArgs e)
+        {
+            chart1.ChartAreas.Add("ChartAreas2");
+            chart1.Series[1].ChartArea = "ChartAreas2";
+
+            BtnMerge.Enabled = true;
+            BtnSplit.Enabled = false;
+        }
+    }
+}
