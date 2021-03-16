af028b854bfa94ee990c1c6f47abed3c97534285
joohy <joohyun104@gmail.com>
2021-03-16 17:27:57 +0900

    0316 BookRentalShop COMMIT

---
+++ WinformApp/BookRentalShopApp/BookRentalShopApp/BookRentalShopApp/FrmLogin.cs
@@ -0,0 +1,73 @@
+﻿using MetroFramework;
+using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookRentalShopApp
+{
+    public partial class FrmLogin : MetroForm
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void BtnLogin_Click(object sender, EventArgs e) //로그인
+        {
+            //MessageBox.Show("로그인");
+            if (string.IsNullOrEmpty(TxtUserId.Text) || string.IsNullOrEmpty(TxtPassword.Text))
+            {
+                MetroMessageBox.Show(this,"아이디/패스워드를 입력하세요", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Helper.Common.ConnString)) //SqlConnection 연결
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+
+                    SqlCommand cmd = new SqlCommand();
+
+                    SqlParameter param; //SQL Injection 방지
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MetroMessageBox.Show(this, ex.Message, "예외발생", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e) //취소
+        {
+            Environment.Exit(0); //완전 종료
+        }
+
+        private void TxtUserId_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) TxtPassword.Focus();
+        }
+
+        private void TxtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) BtnLogin_Click(sender, e);
+        }
+
+        
+    }
+}
---
+++ WinformApp/BookRentalShopApp/BookRentalShopApp/BookRentalShopApp/Metroform.cs
@@ -0,0 +1,6 @@
+﻿namespace BookRentalShopApp
+{
+    public class Metroform
+    {
+    }
+}
